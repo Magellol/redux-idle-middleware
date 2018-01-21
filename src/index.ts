86ca54fada400ee0d1dd7139647e1b078b6e6b19
Thomas Lefebvre <lefebvre515@gmail.com>
2018-01-21 15:39:23 -0500

    Build configuration

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './idle-controller';
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+const config = {
+  devtool: 'source-map',
+  entry: resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'redux-idle.js',
+    library: 'ReduxIdle',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'tslint-loader',
+            options: {
+              configFile: resolve(__dirname, 'tslint.json'),
+              emitErrors: true,
+              failOnHint: true,
+              typeCheck: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   comments: false,
+    //   sourceMap: true,
+    // }),
+  ],
+};
+
+module.exports = config;
